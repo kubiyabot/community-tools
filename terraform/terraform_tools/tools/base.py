2147f7a63516efc809f01c876238e2aa1b2645fd
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-09-11 00:00:07 +0300

    added more tools

--- terraform/terraform_tools/tools/base.py
+++ terraform/terraform_tools/tools/base.py
@@ -1,9 +1,24 @@
-from kubiya_sdk.tools import Tool
+from kubiya_sdk.tools import Tool, FileSpec
 
 TERRAFORM_ICON_URL = "https://www.terraform.io/assets/images/logo-terraform-main.svg"
 
 class TerraformTool(Tool):
     def __init__(self, name, description, content, args, long_running=False):
+        env = [
+            "TF_VAR_file",
+            "TF_CLI_CONFIG_FILE",
+            "TF_LOG",
+            "TF_INPUT",
+            "TF_IN_AUTOMATION",
+            "TF_REGISTRY_DISCOVERY_RETRY",
+            "TF_REGISTRY_CLIENT_TIMEOUT",
+        ]
+
+        files = [
+            FileSpec(source="$HOME/.aws/credentials", destination="/root/.aws/credentials"),
+            FileSpec(source="$HOME/.aws/config", destination="/root/.aws/config"),
+        ]
+
         super().__init__(
             name=name,
             description=description,
@@ -12,5 +27,15 @@ class TerraformTool(Tool):
             image="hashicorp/terraform:latest",
             content=content,
             args=args,
-            long_running=long_running
+            long_running=long_running,
+            env=env,
+            files=files,
         )
+
+    def with_github_token(self):
+        self.env.append("GH_TOKEN")
+        return self
+
+    def with_aws_credentials(self):
+        self.env.extend(["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_SESSION_TOKEN"])
+        return self
