593d5d8f3f2b2630393948eddb91b3125ed1ed67
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-09-12 09:26:42 +0300

    fixed jira

--- jira/jira_tools/tools/__init__.py
+++ jira/jira_tools/tools/__init__.py
@@ -1,22 +1,3 @@
 from .issues import *
 from .epics import *
 from .sprints import *
-
-__all__ = [
-    'issue_list',
-    'issue_create',
-    'issue_edit',
-    'issue_delete',
-    'issue_assign',
-    'issue_move',
-    'issue_view',
-    'issue_comment_add',
-    'issue_link',
-    'issue_watch',
-    'epic_list',
-    'epic_create',
-    'epic_add',
-    'epic_remove',
-    'sprint_list',
-    'sprint_add',
-]
