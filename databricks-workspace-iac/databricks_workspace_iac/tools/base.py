11c5e454507be7520fc021a0f41bfc3930d95db7
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-09-17 14:26:56 +0300

    enhance databricks tooling

--- databricks-workspace-iac/databricks_workspace_iac/tools/base.py
+++ databricks-workspace-iac/databricks_workspace_iac/tools/base.py
@@ -1,5 +1,5 @@
 from kubiya_sdk.tools.models import Tool
-from ..constants import DATABRICKS_ICON_URL, AWS_FILES
+from .constants import DATABRICKS_ICON_URL, AWS_FILES, AWS_ENV, AZURE_ENV
 
 class DatabricksTerraformTool(Tool):
     def __init__(self, name, description, content, args, env, long_running=True, with_files=None):
@@ -25,5 +25,18 @@ class DatabricksAWSTerraformTool(DatabricksTerraformTool):
             args=args,
             env=env,
             long_running=long_running,
-            with_files=AWS_FILES
+            with_files=AWS_FILES,
+            env=AWS_ENV,
+        )
+
+class DatabricksAzureTerraformTool(DatabricksTerraformTool):
+    def __init__(self, name, description, content, args, env, long_running=True):
+        super().__init__(
+            name=name,
+            description=description,
+            content=content,
+            args=args,
+            env=AZURE_ENV,
+            long_running=long_running,
+            #with_files=AZURE_FILES, ## Kubiya does not support native integration with Azure yet, it is possible to use environment variables / secrets to pass in the execution environment. (eg. Team mate settings)
         )
---
+++ databricks-workspace-iac/databricks_workspace_iac/tools/constants.py
@@ -0,0 +1,38 @@
+from kubiya_sdk.tools.models import FileSpec
+
+AWS_ENV = [
+    "DB_ACCOUNT_ID", # The Databricks Account ID
+    "DB_ACCOUNT_CLIENT_ID", # The Databricks Client ID
+    "DB_ACCOUNT_CLIENT_SECRET", # The Databricks Client Secret
+    "GIT_ORG", # The GitHub Organization
+    "GIT_REPO", # The GitHub Repository
+    "BRANCH", # The branch to use
+    "DIR", # The directory to use
+    "PAT", # The PAT to use
+    "AWS_PROFILE" # The AWS Profile to use
+]
+
+AZURE_ENV = [
+    "DB_ACCOUNT_ID",
+    "DB_ACCOUNT_CLIENT_ID", # The Databricks Client ID
+    "DB_ACCOUNT_CLIENT_SECRET", # The Databricks Client Secret
+    "GIT_ORG", # The GitHub Organization
+    "GIT_REPO", # The GitHub Repository
+    "BRANCH", # The branch to use
+    "DIR", # The directory to use
+    "PAT", # The PAT to use
+    "ARM_CLIENT_ID", # The ARM Client ID (Set via Team Mate settings)
+    "ARM_CLIENT_SECRET", # The ARM Client Secret (Set via Team Mate settings)
+    "ARM_TENANT_ID", # The ARM Tenant ID (Set via Team Mate settings)
+    "ARM_SUBSCRIPTION_ID", # The ARM Subscription ID
+    "SLACK_CHANNEL_ID", # The Slack Channel ID
+    "SLACK_THREAD_TS", # The Slack Thread Timestamp
+    "SLACK_API_TOKEN" # The Slack API Token
+]
+
+DATABRICKS_ICON_URL = "https://www.databricks.com/wp-content/uploads/2020/04/databricks-logo-1.png"
+
+AWS_FILES = [
+    FileSpec(source="$HOME/.aws/credentials", destination="/root/.aws/credentials"), # The AWS Credentials file - must be set to the location of the AWS credentials file on the host machine
+    FileSpec(source="$HOME/.aws/config", destination="/root/.aws/config"), # The AWS Config file - must be set to the location of the AWS config file on the host machine
+]
