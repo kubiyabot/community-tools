190875a571a6d25dd40596ad6c05bdb9a664c947
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-09-19 09:24:29 +0300

    enhance databricks tooling

--- databricks-workspace-iac/databricks_workspace_iac/tools/base.py
+++ databricks-workspace-iac/databricks_workspace_iac/tools/base.py
@@ -1,5 +1,5 @@
 from kubiya_sdk.tools.models import Tool
-from .constants import DATABRICKS_ICON_URL, AWS_FILES, AWS_ENV, AZURE_ENV
+from .constants import DATABRICKS_ICON_URL, AWS_ENV, AZURE_ENV
 
 class DatabricksTerraformTool(Tool):
     def __init__(self, name, description, content, args, env, long_running=True, with_files=None, image="hashicorp/terraform:latest", mermaid=None):
@@ -8,7 +8,7 @@ class DatabricksTerraformTool(Tool):
             description=description,
             icon_url=DATABRICKS_ICON_URL,
             type="docker",
-            image="hashicorp/terraform:latest",
+            image=image,
             content=content,
             args=args,
             env=env,
@@ -25,7 +25,6 @@ class DatabricksAWSTerraformTool(DatabricksTerraformTool):
             content=content,
             args=args,
             long_running=long_running,
-            with_files=AWS_FILES,
             env=AWS_ENV,
             mermaid=mermaid,
         )
@@ -40,5 +39,4 @@ class DatabricksAzureTerraformTool(DatabricksTerraformTool):
             env=AZURE_ENV,
             long_running=long_running,
             mermaid=mermaid,
-            #with_files=AZURE_FILES, ## Kubiya does not support native integration with Azure yet, it is possible to use environment variables / secrets to pass in the execution environment. (eg. Team mate settings)
         )
