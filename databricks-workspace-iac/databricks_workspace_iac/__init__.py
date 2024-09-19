5c54e605150aced17f2bf5003f7e3f4f5aec9b7c
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-09-19 10:05:00 +0300

    enhance databricks tooling

--- databricks-workspace-iac/databricks_workspace_iac/__init__.py
+++ databricks-workspace-iac/databricks_workspace_iac/__init__.py
@@ -1,3 +1,1 @@
-from .tools import aws_db_apply_tool, azure_db_apply_tool
-from .models import WorkspaceConfig
-from .utils import generate_terraform_vars, generate_backend_config
+from .tools import *
