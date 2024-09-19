959ca3a41a90160e1ab6d9fdb1bbc753539c3795
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-09-19 13:55:16 +0300

    enhance databricks tooling

--- databricks-workspace-iac/databricks_workspace_iac/tools/azure/settings.py
+++ databricks-workspace-iac/databricks_workspace_iac/tools/azure/settings.py
@@ -129,7 +129,8 @@ AZURE_TEMPLATE_PARAMS = {
     "CHECK_REQUIRED_VARS": CHECK_REQUIRED_VARS_COMMANDS,
     
     # Terraform init command with Azure backend configuration
-    "TERRAFORM_INIT_COMMAND": 'terraform init -backend-config="storage_account_name=${storage_account_name}" -backend-config="container_name=${container_name}" -backend-config="key=databricks/${WORKSPACE_NAME}/terraform.tfstate" -backend-config="resource_group_name=${resource_group_name}" -backend-config="subscription_id=$ARM_SUBSCRIPTION_ID"',
+    # Double curly braces are used to interpolate variables in the command
+    "TERRAFORM_INIT_COMMAND": 'terraform init -backend-config="storage_account_name=${{storage_account_name}}" -backend-config="container_name=${container_name}" -backend-config="key=databricks/${{WORKSPACE_NAME}}/terraform.tfstate" -backend-config="resource_group_name=${{resource_group_name}}""',
     
     # JSON representation of Terraform variables
     "TERRAFORM_VARS_JSON": generate_terraform_vars_json(TF_VARS),
