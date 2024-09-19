75fe62bde1c8174774f30784d6d1e96230e7329f
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-09-19 10:06:30 +0300

    enhance databricks tooling

--- databricks-workspace-iac/databricks_workspace_iac/tools/__init__.py
+++ databricks-workspace-iac/databricks_workspace_iac/tools/__init__.py
@@ -1,15 +1,2 @@
-from .azure import azure_db_apply_tool
-from .aws import aws_db_apply_tool
-from .base import DatabricksTerraformTool, DatabricksAWSTerraformTool, DatabricksAzureTerraformTool
-from .constants import AZURE_ENV, AWS_ENV, DATABRICKS_ICON_URL
-
-__all__ = [
-    'azure_db_apply_tool',
-    'aws_db_apply_tool',
-    'DatabricksTerraformTool',
-    'DatabricksAWSTerraformTool',
-    'DatabricksAzureTerraformTool',
-    'AZURE_ENV',
-    'AWS_ENV',
-    'DATABRICKS_ICON_URL'
-]
+from .azure import *
+from .aws import *
