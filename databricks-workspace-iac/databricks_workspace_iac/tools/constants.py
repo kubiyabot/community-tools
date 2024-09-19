d8a64abf7defe81e7556a63835d73f491cf07bcd
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-09-19 09:24:29 +0300

    enhance databricks tooling

--- databricks-workspace-iac/databricks_workspace_iac/tools/constants.py
+++ databricks-workspace-iac/databricks_workspace_iac/tools/constants.py
@@ -3,4 +3,7 @@ import os
 AZURE_ENV = os.environ
 AWS_ENV = os.environ
 
+# Add this line
+DATABRICKS_ICON_URL = "https://www.databricks.com/wp-content/uploads/2020/04/databricks-logo-1.png"
+
 # You might want to add any specific environment variables here if needed
