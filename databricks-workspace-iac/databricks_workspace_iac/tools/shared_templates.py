28ca40017c6e01634eb0ad5d69e9acf93376671c
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-09-19 16:46:15 +0300

    enhance databricks tooling

--- databricks-workspace-iac/databricks_workspace_iac/tools/shared_templates.py
+++ databricks-workspace-iac/databricks_workspace_iac/tools/shared_templates.py
@@ -21,7 +21,7 @@ def generate_terraform_vars_json(tf_vars):
         if default is not None:
             value = default
         else:
-            value = "${" + name + "}"
+            value = f"${{{name}}}"
         # Try to parse the default value as JSON
         try:
             # This will handle booleans, numbers, lists, and nulls
