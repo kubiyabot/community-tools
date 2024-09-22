a96e744b781c17672d595a4842520507d64ad070
omerc7 <omerc7@gmail.com>
2024-09-22 18:25:53 +0300

    azure add args

--- databricks-workspace-iac/databricks_workspace_iac/tools/azure/settings.py
+++ databricks-workspace-iac/databricks_workspace_iac/tools/azure/settings.py
@@ -195,15 +195,15 @@ AZURE_TEMPLATE_PARAMS = {
     "CHECK_REQUIRED_VARS": CHECK_REQUIRED_VARS_COMMANDS,
     # Terraform init command with Azure backend configuration
     # Double curly braces are used to interpolate variables in the command
-    "TERRAFORM_INIT_COMMAND": 'terraform init -backend-config="storage_account_name=${storage_account_name}" -backend-config="container_name=${{container_name}}" -backend-config="key=databricks/${{WORKSPACE_NAME}}/terraform.tfstate" -backend-config="resource_group_name=${{resource_group_name}}" -backend-config="subscription_id=${{ARM_SUBSCRIPTION_ID}}"',
+    "TERRAFORM_INIT_COMMAND": 'terraform init -backend-config="storage_account_name=${storage_account_name}" -backend-config="container_name=${container_name}" -backend-config="key=databricks/${workspace_name}/terraform.tfstate" -backend-config="resource_group_name=${resource_group_name}" -backend-config="subscription_id=${{ARM_SUBSCRIPTION_ID}}"',
     # JSON representation of Terraform variables
     "TERRAFORM_VARS_JSON": generate_terraform_vars_json(TF_VARS),
     # Fallback URL for the V_VARS + REQUIRED_SECRETSDatabricks workspace if direct URL is unavailable
-    "FALLBACK_WORKSPACE_URL": "https://portal.azure.com/#@/resource/subscriptions/${{ARM_SUBSCRIPTION_ID}}/resourceGroups/${{resource_group_name}}/providers/Microsoft.Databricks/workspaces/${{WORKSPACE_NAME}}",
+    "FALLBACK_WORKSPACE_URL": "https://portal.azure.com/#@/resource/subscriptions/${{ARM_SUBSCRIPTION_ID}}/resourceGroups/${resource_group_name}/providers/Microsoft.Databricks/workspaces/${workspace_name}",
     # Specifies the backend type for Terraform state (azurerm for Azure)
     "BACKEND_TYPE": "azurerm",
     # Command to import existing Databricks workspace into Terraform state
-    "IMPORT_COMMAND": "terraform import azurerm_databricks_workspace.this /subscriptions/${{ARM_SUBSCRIPTION_ID}}/resourceGroups/${{resource_group_name}}/providers/Microsoft.Databricks/workspaces/${{WORKSPACE_NAME}}",
+    "IMPORT_COMMAND": "terraform import azurerm_databricks_workspace.this /subscriptions/${{ARM_SUBSCRIPTION_ID}}/resourceGroups/${resource_group_name}/providers/Microsoft.Databricks/workspaces/${workspace_name}",
     # Name of the Git repository containing the Terraform configurations
     "GIT_REPO": GIT_REPO,
 }
--- databricks-workspace-iac/databricks_workspace_iac/tools/azure/workspace.py
+++ databricks-workspace-iac/databricks_workspace_iac/tools/azure/workspace.py
@@ -22,6 +22,25 @@ tf_args = [
     for var in TF_VARS
     if var["default"] is not None
 ]
+tf_args.extend(
+    [
+        Arg(
+            name="container_name",
+            description="The name of the container to use for the backend",
+            required=True,
+        ),
+        Arg(
+            name="workspace_name",
+            description="The name of the Databricks workspace to be created",
+            required=True,
+        ),
+        Arg(
+            name="resource_group_name",
+            description="The name of the resource group to use for the backend",
+            required=True,
+        ),
+    ]
+)
 
 azure_db_apply_tool = DatabricksAzureTerraformTool(
     name="create-databricks-workspace-on-azure",
