26c70c8ff341bf9bcfb3cfea1bd0c4629b27893e
EvgeniyReich <evgeniy.reichelson@kubiya.ai>
2024-09-24 12:38:49 +0300

    IT207 added setup.py, Dockerfile

---
+++ elastic-search/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="report-tool",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.0",
+        "boto3>=1.38.0",
+    ],
+    entry_points={
+        'console_scripts': [
+            'create_es_watchers=report_tool.tools.report:create_es_watchers',
+        ],
+    },
+)
