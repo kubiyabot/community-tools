41d7b6936c33830f0f4c6ffcc4b983ff327a5c18
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-10-04 15:08:31 +0300

    k8s tools

--- kubernetes/k8s_tools/tools/__init__.py
+++ kubernetes/k8s_tools/tools/__init__.py
@@ -1,18 +1,45 @@
 # k8s_tools/tools/__init__.py
 
-from .kubectl import kubectl_tool
-from .deployment import deployment_tool
 from .service import service_tool
 from .pod import pod_tool
-from .insights import resource_usage_tool, cluster_health_tool
-from .automations import scale_deployment_tool
+from .deployment import deployment_tool
+from .automations import (
+    scale_deployment_tool,
+    find_resource_tool,
+    change_replicas_tool,
+    get_resource_events_tool,
+    get_resource_logs_tool,
+    cluster_health_tool,
+    node_status_tool,
+    find_suspicious_errors_tool,
+    resource_usage_tool,
+    check_pod_restarts_tool,
+    network_policy_analyzer_tool,
+    persistent_volume_usage_tool,
+    ingress_analyzer_tool,
+    resource_quota_usage_tool,
+    cluster_autoscaler_status_tool,
+    pod_disruption_budget_checker_tool,
+)
 
 __all__ = [
-    'kubectl_tool',
-    'deployment_tool',
-    'service_tool',
-    'pod_tool',
-    'resource_usage_tool',
-    'cluster_health_tool',
-    'scale_deployment_tool',
+    "service_tool",
+    "pod_tool",
+    "deployment_tool",
+    "scale_deployment_tool",
+    "find_resource_tool",
+    "change_replicas_tool",
+    "get_resource_events_tool",
+    "get_resource_logs_tool",
+    "cluster_health_tool",
+    "node_status_tool",
+    "find_suspicious_errors_tool",
+    "resource_usage_tool",
+    "check_pod_restarts_tool",
+    "network_policy_analyzer_tool",
+    "persistent_volume_usage_tool",
+    "ingress_analyzer_tool",
+    "resource_quota_usage_tool",
+    "cluster_autoscaler_status_tool",
+    "pod_disruption_budget_checker_tool",
 ]
