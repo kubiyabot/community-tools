0b424f77bca146c7947c6948107a2396e58a6691
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-10-04 15:51:09 +0300

    override default container entrypoint - fix

--- kubernetes/k8s_tools/tools/base.py
+++ kubernetes/k8s_tools/tools/base.py
@@ -13,10 +13,10 @@ CERT_LOCATION="/tmp/kubernetes_context_cert"
 if [ -f $TOKEN_LOCATION ] && [ -f $CERT_LOCATION ]; then
     KUBE_TOKEN=$(cat $TOKEN_LOCATION)
     kubectl config set-cluster in-cluster --server=https://kubernetes.default.svc \
-                                          --certificate-authority=$CERT_LOCATION
-    kubectl config set-credentials in-cluster --token=$KUBE_TOKEN
-    kubectl config set-context in-cluster --cluster=in-cluster --user=in-cluster
-    kubectl config use-context in-cluster
+                                          --certificate-authority=$CERT_LOCATION > /dev/null 2>&1
+    kubectl config set-credentials in-cluster --token=$KUBE_TOKEN > /dev/null 2>&1
+    kubectl config set-context in-cluster --cluster=in-cluster --user=in-cluster > /dev/null 2>&1
+    kubectl config use-context in-cluster > /dev/null 2>&1
 else
     echo "Error: Kubernetes context token or cert file not found at $TOKEN_LOCATION \
           or $CERT_LOCATION respectively."
