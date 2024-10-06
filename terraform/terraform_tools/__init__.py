174f59293cc3ff6e74f1a1a3aa00eaadf1504835
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-10-06 19:59:39 +0300

    enhance TF tooling

--- terraform/terraform_tools/__init__.py
+++ terraform/terraform_tools/__init__.py
@@ -1,27 +1,3 @@
-from .tools.terraform_operations import (
-    terraform_init,
-    terraform_plan,
-    terraform_apply,
-    terraform_destroy,
-    terraform_output,
-    terraform_show,
-    terraform_validate,
-    terraform_workspace_list,
-    terraform_workspace_select,
-    terraform_state_list,
-    terraform_state_show
-)
+from .tools.terraform_operations import *
 
-__all__ = [
-    'terraform_init',
-    'terraform_plan',
-    'terraform_apply',
-    'terraform_destroy',
-    'terraform_output',
-    'terraform_show',
-    'terraform_validate',
-    'terraform_workspace_list',
-    'terraform_workspace_select',
-    'terraform_state_list',
-    'terraform_state_show'
-]
+__all__ = [name for name in dir() if name.startswith('terraform_')]
