b1393e9ca9c705b7e8ce4effc79a3b13c3eabcff
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-10-06 16:19:32 +0300

    enhance TF tooling

--- terraform/terraform_tools/__init__.py
+++ terraform/terraform_tools/__init__.py
@@ -1,19 +1,27 @@
-from .tools.apply import terraform_apply_tool
-from .tools.destroy import terraform_destroy_tool
-from .tools.module import terraform_module_tool
-from .tools.output import terraform_output_tool
-from .tools.state import terraform_state_tool
-from .tools.workspace import terraform_workspace_tool
-from .tools.workflow import terraform_workflow_tool
-from .tools.git import terraform_git_clone
+from .tools.terraform_operations import (
+    terraform_init,
+    terraform_plan,
+    terraform_apply,
+    terraform_destroy,
+    terraform_output,
+    terraform_show,
+    terraform_validate,
+    terraform_workspace_list,
+    terraform_workspace_select,
+    terraform_state_list,
+    terraform_state_show
+)
 
 __all__ = [
-    'terraform_apply_tool',
-    'terraform_destroy_tool',
-    'terraform_module_tool',
-    'terraform_output_tool',
-    'terraform_state_tool',
-    'terraform_workspace_tool',
-    'terraform_workflow_tool',
-    'terraform_git_clone',
+    'terraform_init',
+    'terraform_plan',
+    'terraform_apply',
+    'terraform_destroy',
+    'terraform_output',
+    'terraform_show',
+    'terraform_validate',
+    'terraform_workspace_list',
+    'terraform_workspace_select',
+    'terraform_state_list',
+    'terraform_state_show'
 ]
