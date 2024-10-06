4490aff9529c959b413473438b9cb2ae1bbdef99
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-10-06 16:41:38 +0300

    enhance TF tooling

--- terraform/terraform_tools/__init__.py
+++ terraform/terraform_tools/__init__.py
@@ -1,4 +1,4 @@
-from .tools.terraform_operations import (
+from terraform.terraform_tools.tools.terraform_operations import (
     terraform_init,
     terraform_plan,
     terraform_apply,
@@ -13,25 +13,15 @@ from .tools.terraform_operations import (
 )
 
 __all__ = [
-    'init',
-    'plan',
-    'apply',
-    'destroy',
-    'output',
-    'show',
-    'validate',
-    'workspace_list',
-    'workspace_select',
-    'state_list',
-    'state_show'
+    'terraform_init',
+    'terraform_plan',
+    'terraform_apply',
+    'terraform_destroy',
+    'terraform_output',
+    'terraform_show',
+    'terraform_validate',
+    'terraform_workspace_list',
+    'terraform_workspace_select',
+    'terraform_state_list',
+    'terraform_state_show'
 ]
-
-# Create a dictionary mapping the short names to the full tool names
-tools_dict = {
-    name.replace('terraform_', ''): globals()[name]
-    for name in globals()
-    if name.startswith('terraform_')
-}
-
-# Update the global namespace with the short names
-globals().update(tools_dict)
