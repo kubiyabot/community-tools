1c0e3bdf6db57d506da61b8e74223365f34dbb2e
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-10-07 10:25:38 +0300

    github tooling

--- github/github_tools/tools/common.py
+++ github/github_tools/tools/common.py
@@ -1,10 +1,8 @@
 from kubiya_sdk.tools import FileSpec
 
 COMMON_ENV = [
-    "GITHUB_TOKEN",
+    "GH_TOKEN", # Github Token (integration) - https://docs.kubiya.ai/integrations/github
 ]
 
 COMMON_FILES = [
-    FileSpec(source="~/.config/gh/config.yml", destination="/root/.config/gh/config.yml"),
-    FileSpec(source="~/.config/gh/hosts.yml", destination="/root/.config/gh/hosts.yml"),
 ]
