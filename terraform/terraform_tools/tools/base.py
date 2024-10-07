d1d632eecc422629fdc2c2fab98da4e527b96ed5
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-10-07 09:00:59 +0300

    enhance tf tooling

--- terraform/terraform_tools/tools/base.py
+++ terraform/terraform_tools/tools/base.py
@@ -5,8 +5,8 @@ TERRAFORM_ICON_URL = "https://www.terraform.io/assets/images/logo-terraform-main
 class TerraformTool(Tool):
     def __init__(self, name, description, content, args, long_running=True):
         file_specs = [
-            FileSpec(source="$HOME/.aws/credentials", destination="/root/.aws/credentials"),
-            FileSpec(source="$HOME/.aws/config", destination="/root/.aws/config")
+            FileSpec(source="$HOME/.aws/credentials", destination="/root/.aws/credentials"), # AWS Credentials (integration) - https://docs.kubiya.ai/integrations/aws
+            FileSpec(source="$HOME/.aws/config", destination="/root/.aws/config") # AWS Credentials (integration) - https://docs.kubiya.ai/integrations/aws
         ]
 
         super().__init__(
@@ -18,7 +18,7 @@ class TerraformTool(Tool):
             content=content,
             args=args,
             long_running=long_running,
-            env=["AWS_PROFILE", "GH_TOKEN"],
-            secrets=["GITHUB_TOKEN"],
+            env=["AWS_PROFILE"], # AWS Profile - used to get AWS credentials (integration) - https://docs.kubiya.ai/integrations/aws
+            secrets=["GH_TOKEN"], # Github Token (integration) - https://docs.kubiya.ai/integrations/github
             with_files=file_specs,
         )
