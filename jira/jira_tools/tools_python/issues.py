d522a6a1922d2f6e92ab0b139254fe77a80451fe
raz-alon <raz.alon@kubiya.ai>
2024-11-03 17:00:40 +0200

    [Change] after fixed

--- jira/jira_tools/tools_python/issues.py
+++ jira/jira_tools/tools_python/issues.py
@@ -9,13 +9,13 @@ create_issue_tool = JiraPythonTool(
     name="create_task",
     description="Create new jira task",
     content="""
-    python /tmp/create_issue.py "{{ .project_key }}" "{{ .name }}" "{{ .description }}" "{{ .priority }}" "{{ .assignee_email }}" "{{ .label }}"
+    
+    python /tmp/create_issue.py "{{ .project_key }}" "{{ .name }}" "{{ .description }}" Task "{{ .priority }}" "{{ .assignee_email }}" "{{ .label }}"
     """,
 
     args=[
         Arg(name="project_key", type="str", description="Jira project key", required=True),
         Arg(name="name", type="str", description="Issue name", required=True),
-        Arg(name="issue_type", value="Task",type="str", description="Issue type, such as Task, Sub-Task, Epic, Bug etc.",required=True),
         Arg(name="description", type="str", description="Issue description", required=True),
         Arg(name="priority", type="str", description="Issue priority can be Low Medium or High", required=False),
         Arg(name="assignee_email", type="str", description="Issue assignee user", required=False),
