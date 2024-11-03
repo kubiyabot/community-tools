a82c7c8623027bfc439acee731073e2b63fb4848
raz-alon <raz.alon@kubiya.ai>
2024-11-03 18:28:23 +0200

    [Change] fix sub-task

--- jira/jira_tools/tools_python/create_issue.py
+++ jira/jira_tools/tools_python/create_issue.py
@@ -43,7 +43,7 @@ def main():
     parser.add_argument("priority", help="Priority of the issue", default=None)
     parser.add_argument("assignee_email", help="Assignee's email address", default=None)
     parser.add_argument("--label", help="Label for the issue", default="")
-    parser.add_argument("--parent", help="parent id for the task", default="")
+    parser.add_argument("--parent_id", help="parent id for the task", default="")
     args = parser.parse_args()
 
     cloud_id = get_jira_cloud_id()
@@ -59,7 +59,7 @@ def main():
     )
 
     if args.parent: # especially for subtasks
-        payload["fields"]["parent"] = {"key": args.parent}
+        payload["fields"]["parent"] = {"key": args.parent_id}
 
     post_issue_url = f"{ATLASSIAN_JIRA_API_URL}/{cloud_id}/rest/api/3/issue"
 
