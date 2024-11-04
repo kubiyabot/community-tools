0440585666c787fbe14e8c6b8d226c9b29b437c5
raz-alon <raz.alon@kubiya.ai>
2024-11-04 12:15:38 +0200

    [ADD] Issues list

--- jira/jira_tools/tools_python/issues.py
+++ jira/jira_tools/tools_python/issues.py
@@ -100,8 +100,8 @@ list_issue_tool = JiraPythonTool(
     description="List Jira issues",
     content="""
 python /tmp/list_issues.py "{{ .project_key }}" --num="{{ .num }}" --status="{{ .status }}" --assignee="{{ .assignee }}" --priority="{{ .priority }}" --reporter="{{ .reporter }}" 
-echo $("{{ .num }})"
-echo $("{{ .status }})"
+echo $("{{ .num }}")
+echo $("{{ .status }}")
 """,
     args=[
         Arg(name="project_key", type="str", description="Jira project key", required=True),
