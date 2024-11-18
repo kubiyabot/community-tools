70b3ede2c7c0f20e2b28eb068726c85c400178eb
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-11-18 16:22:03 +0200

    adjusted to py package

--- just_in_time_access/__init__.py
+++ just_in_time_access/__init__.py
@@ -1,9 +1,13 @@
-from .src.tools.approve_access import approve_access_tool
-from .src.tools.request_access import request_access_tool
-from .src.tools.describe_access_request import describe_access_request_tool
+from .src.tools import (
+    approve_access_tool,
+    request_access_tool,
+    describe_access_request_tool,
+    list_active_access_requests_tool,
+)
 
 __all__ = [
     'approve_access_tool',
     'request_access_tool',
     'describe_access_request_tool',
+    'list_active_access_requests_tool',
 ]
