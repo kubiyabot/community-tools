46fb2296a5ee7d772b144c27e9944ee4c63f87e2
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-11-19 18:59:27 +0200

    aws logo

--- aws_jit_tools/aws_jit_tools/tools/base.py
+++ aws_jit_tools/aws_jit_tools/tools/base.py
@@ -1,7 +1,7 @@
 from kubiya_sdk.tools.models import Tool
 from .common import COMMON_FILES, COMMON_ENV
 
-AWS_JIT_ICON = "https://d2908q01vomqb2.cloudfront.net/22d200f8670dbdb3e253a90eee5098477c95c23d/2018/09/24/aws-icon-service-IAM_PERMISSIONS.png"
+AWS_JIT_ICON = "https://img.icons8.com/color/200/amazon-web-services.png"
 
 class AWSJITTool(Tool):
     """Base class for AWS JIT access tools."""
