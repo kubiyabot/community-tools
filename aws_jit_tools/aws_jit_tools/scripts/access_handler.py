9f48ce986d8e587102ba2898eed7269881ccf6cd
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-11-20 16:58:36 +0200

    fix

--- aws_jit_tools/aws_jit_tools/scripts/access_handler.py
+++ aws_jit_tools/aws_jit_tools/scripts/access_handler.py
@@ -49,7 +49,6 @@ except ImportError as e:
 from scripts.utils.notifications import NotificationManager
 from scripts.utils.aws_utils import get_account_alias, get_permission_set_details
 from scripts.utils.slack_messages import create_access_revoked_blocks
-from scripts.utils.iam_policy_manager import IAMPolicyManager
 from scripts.utils.webhook_handler import WebhookHandler
 
 def print_progress(message: str, emoji: str) -> None:
