451c84055c83f63cb647870b673a5effc7274e78
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-12-05 20:28:28 +0200

    k8s enhacments

--- kubernetes/k8s_tools/utils/init_cluster.sh
+++ kubernetes/k8s_tools/utils/init_cluster.sh
@@ -113,8 +113,8 @@ if ! helm list -n kubiya | grep -q "kubiya-kubewatch"; then
         --namespace kubiya \
         --create-namespace \
         --set rbac.create=true \
-        --set image.repository=ghcr.io/kubiyabot/kubewatch \
-        --set image.tag=latest \
+        --set image.repository=ghcr.io/kubiyabot/kubewatch:gha-build \
+        --set image.tag=dd496405c04f285b3d164f11e4463b3ea02e6ea1 \
         -f "$VALUES_FILE"
     check_command "kubiya-kubewatch installation failed" "Initiated kubiya-kubewatch installation"
 else
@@ -123,6 +123,8 @@ else
     helm upgrade kubiya-kubewatch robusta/kubewatch \
         --namespace kubiya \
         --reuse-values \
+        --set image.repository=ghcr.io/kubiyabot/kubewatch:gha-build \
+        --set image.tag=dd496405c04f285b3d164f11e4463b3ea02e6ea1 \
         -f "$VALUES_FILE"
     check_command "kubiya-kubewatch upgrade failed" "Updated kubiya-kubewatch configuration"
 fi
