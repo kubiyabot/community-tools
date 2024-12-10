6d2c2bd2e562485ede93abf46107a01301a2f38e
Shaked Askayo <35655764+shakedaskayo@users.noreply.github.com>
2024-12-10 20:43:33 +0200

    k8s enhacments

--- kubernetes/k8s_tools/tools/__init__.py
+++ kubernetes/k8s_tools/tools/__init__.py
@@ -1,15 +1,66 @@
 # Import all tools
 from .kubectl import kubectl_tool
-from .deployment import deployment_tool
-from .service import service_tool
-from .pv import pv_tool
-from .pvc import pvc_tool
-from .automations import *
+from .deployment import deployment_create, deployment_delete, deployment_get, deployment_update
+from .service import service_create, service_delete, service_get
+from .pv import pv_create, pv_delete, pv_get
+from .pvc import pvc_create, pvc_delete, pvc_get
+from .pod import pod_get, pod_delete, pod_logs
+from .automations import (
+    find_resource_tool,
+    change_replicas_tool,
+    get_resource_events_tool,
+    get_pod_logs_tool,
+    node_status_tool,
+    find_suspicious_errors_tool,
+    network_policy_analyzer_tool,
+    persistent_volume_usage_tool,
+    ingress_analyzer_tool,
+    resource_quota_usage_tool,
+    cluster_autoscaler_status_tool,
+    pod_disruption_budget_checker_tool,
+    check_replicas_tool
+)
 
 __all__ = [
+    # Core tools
     'kubectl_tool',
-    'deployment_tool',
-    'service_tool',
-    'pv_tool',
-    'pvc_tool',
+    
+    # Deployment tools
+    'deployment_create',
+    'deployment_delete',
+    'deployment_get',
+    'deployment_update',
+    
+    # Service tools
+    'service_create',
+    'service_delete',
+    'service_get',
+    
+    # Storage tools
+    'pv_create',
+    'pv_delete',
+    'pv_get',
+    'pvc_create',
+    'pvc_delete',
+    'pvc_get',
+    
+    # Pod tools
+    'pod_get',
+    'pod_delete',
+    'pod_logs',
+    
+    # Automation tools
+    'find_resource_tool',
+    'change_replicas_tool',
+    'get_resource_events_tool',
+    'get_pod_logs_tool',
+    'node_status_tool',
+    'find_suspicious_errors_tool',
+    'network_policy_analyzer_tool',
+    'persistent_volume_usage_tool',
+    'ingress_analyzer_tool',
+    'resource_quota_usage_tool',
+    'cluster_autoscaler_status_tool',
+    'pod_disruption_budget_checker_tool',
+    'check_replicas_tool'
 ]
