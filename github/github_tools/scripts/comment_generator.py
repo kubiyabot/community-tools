f1557ca8e34f1039a5f8faae96e37dfdbc9679b0
michaelkubi <michael.bauer@kubiya.ai>
2024-12-23 18:18:19 +0200

    move dir

--- github/github_tools/scripts/comment_generator.py
+++ github/github_tools/scripts/comment_generator.py
@@ -7,7 +7,7 @@ import json
 import logging
 from pathlib import Path
 
-from .templating.template_handler import TemplateHandler
+from .template_handler import TemplateHandler
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
