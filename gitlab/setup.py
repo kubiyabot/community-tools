c74bd32021a687d942af812734670a62ad4c260a
michagonzo77 <michael.gonz.fl@gmail.com>
2025-03-31 11:00:07 -0600

    New toolset: Gitlab - projects

---
+++ gitlab/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gitlab_tools",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "kubiya-sdk>=0.1.0",
+        "python-gitlab>=3.15.0",
+        "requests>=2.31.0",
+    ],
+    author="Kubiya",
+    description="GitLab management tools for Kubiya",
+    python_requires=">=3.8",
+) 
