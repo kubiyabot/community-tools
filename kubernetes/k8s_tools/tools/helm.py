ad84265c95ae5b5d459a3ca6fb070d93de396a95
michaelkubi <michael.bauer@kubiya.ai>
2025-05-20 19:10:13 +0300

    using default image

--- kubernetes/k8s_tools/tools/helm.py
+++ kubernetes/k8s_tools/tools/helm.py
@@ -50,8 +50,7 @@ helm_list_recent = KubernetesTool(
             type="int",
             default="1"
         )
-    ],
-    image="alpine/helm"  # Using an image with Helm installed
+    ]
 )
 
 # Register the tool
